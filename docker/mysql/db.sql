5518f83076bfb65f0b5437dc6a6f0fd456011674
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-08 00:20:14 -0300

    chore(db): :wrench: update sql to mysql syntax

--- docker/mysql/db.sql
+++ docker/mysql/db.sql
@@ -1,5 +1,3 @@
-CREATE USER febrapar_agenda_api WITH
-    LOGIN
-    PASSWORD 'Secret*123';
-CREATE DATABASE "febrapar_agenda_db" WITH OWNER "febrapar_agenda_api" ENCODING 'UTF8';
-GRANT ALL PRIVILEGES ON DATABASE febrapar_agenda_db TO febrapar_agenda_api;
+CREATE DATABASE febrapar_agenda_db;
+CREATE USER 'febrapar_agenda_api'@'%' IDENTIFIED BY 'Secret*123';
+GRANT ALL PRIVILEGES ON febrapar_agenda_db.* TO 'febrapar_agenda_api'@'%';
