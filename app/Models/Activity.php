a3fbd35aafdbe06a02e720dd379d7f0c8923231d
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-08 13:34:58 -0300

    feat(activity): :sparkles: add activity overlap check and filter scope

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -23,4 +23,37 @@ class Activity extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public static function checkOverlapping($userId, $startDate, $endDate)
+    {
+        $query = self::where('user_id', $userId)
+            ->where(function ($query) use ($startDate, $endDate) {
+                if ($startDate && $endDate) {
+                    $query->where(function ($query) use ($startDate, $endDate) {
+                        $query->whereBetween('start_date', [$startDate, $endDate])
+                            ->orWhereBetween('due_date', [$startDate, $endDate]);
+                    });
+                } elseif ($startDate) {
+                    $query->where('start_date', '>=', $startDate);
+                } elseif ($endDate) {
+                    $query->where('due_date', '<=', $endDate);
+                }
+
+            });
+
+        return $query->exists();
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        $query->when($filters['startDate'], function ($query, $startDate) {
+            $query->where('start_date', '>=', $startDate);
+        });
+
+        $query->when($filters['endDate'], function ($query, $endDate) {
+            $query->where('start_date', '<=', $endDate);
+        });
+
+        return $query;
+    }
 }
