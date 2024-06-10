7c575a38c89902fbe55bc21ebac554951e47b93d
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 09:36:50 -0300

    test: :test_tube: add unit tests for AuthCredentialsDTO

---
+++ tests/Unit/AuthCredentialsDtoTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\DTO\Auth\AuthCredentialsDTO;
+use PHPUnit\Framework\TestCase;
+
+class AuthCredentialsDTOTest extends TestCase
+{
+    private $authCredentialsDTO;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->authCredentialsDTO = new AuthCredentialsDTO('test@example.com', 'password123');
+    }
+
+    public function testGetEmail(): void
+    {
+        $this->assertEquals('test@example.com', $this->authCredentialsDTO->email);
+    }
+
+    public function testGetPassword(): void
+    {
+        $this->assertEquals('password123', $this->authCredentialsDTO->password);
+    }
+}
