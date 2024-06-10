3d0d24a4ed3d1742286140319b02be1f30b26bc6
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 09:31:56 -0300

    test: :test_tube: improve auth tests for reliability and coverage

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -5,13 +5,15 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
-    public function test_register_user(): void
+    public function testRegister()
     {
         $data = [
             'name' => $this->faker->name,
@@ -22,48 +24,46 @@ class AuthControllerTest extends TestCase
 
         $response = $this->postJson('/api/auth/register', $data);
 
-        $response->assertStatus(201)
-            ->assertJsonStructure(['user' => ['id', 'name', 'email']]);
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('users', ['email' => $data['email']]);
     }
 
-    public function test_login_user(): void
+    public function testAuthenticate()
     {
-        /**
-         * @var User $user
-         */
+        $password = 'password';
         $user = User::factory()->create([
             'email' => $this->faker->unique()->safeEmail,
-            'password' => bcrypt('password'),
+            'password' => Hash::make($password),
         ]);
 
         $data = [
             'email' => $user->email,
-            'password' => 'password',
+            'password' => $password,
         ];
 
         $response = $this->postJson('/api/auth/login', $data);
 
-        $response->assertStatus(200)
-            ->assertJsonStructure(['token']);
+        $response->assertStatus(200);
+        $this->assertAuthenticatedAs($user);
     }
 
-    public function test_logout_user(): void
+    public function testLogout()
     {
-        /**
-         * @var User $user
-         */
-        $user = User::factory()->create();
-        /**
-         * @var \Laravel\Sanctum\NewAccessToken $token
-         */
-        $token = $user->createToken('auth_token');
-        $textToken = $token->plainTextToken;
-
-        $response = $this->postJson('/api/auth/logout', [], [
-            'Authorization' => "Bearer $textToken",
+        $password = 'password';
+        $user = User::factory()->create([
+            'password' => Hash::make($password),
         ]);
 
-        $response->assertStatus(200)
-            ->assertJson(['message' => 'Logged out successfully']);
+        Sanctum::actingAs($user, ['*']); // Configura o usuário autenticado usando Sanctum
+
+        $response = $this->postJson('/api/auth/logout');
+
+        $response->assertStatus(200);
+
+        // Verificar se o token foi revogado
+        $this->assertDatabaseMissing('personal_access_tokens', [
+            'tokenable_id' => $user->id,
+            'tokenable_type' => User::class,
+        ]);
     }
 }
