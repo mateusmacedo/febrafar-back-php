bddc716e7b75e74f6a1d7c671cbfa97d97855842
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 04:00:08 -0300

    refactor: :hammer: relocate activities route under api group

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::apiResource('activities', ActivityController::class);
 });
-
-Route::apiResource('activities', ActivityController::class);
