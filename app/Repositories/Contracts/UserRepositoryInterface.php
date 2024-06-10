ed0c0962c3aef107510427cd33a9891c78ef436a
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 09:26:24 -0300

    feat: :sparkles: add user repository interface and implementation

---
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Models\User;
+
+interface UserRepositoryInterface
+{
+    /**
+     * Save a user.
+     * @param User $user
+     * @return User
+     */
+    public function save(User $user): User;
+
+    /**
+     * Find a user by email.
+     * @param string $email
+     * @return User|null
+     */
+    public function findByEmail(string $email): ?User;
+
+    /**
+     * Delete a user.
+     * @param User $user
+     * @return void
+     */
+    public function delete(User $user): void;
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Repositories\Contracts\UserRepositoryInterface;
+
+/**
+ * Class UserRepository
+ * @package App\Repositories
+ */
+class UserRepository implements UserRepositoryInterface
+{
+    /**
+     * Save a user.
+     * @param User $user
+     * @return User
+     */
+    public function save(User $user): User
+    {
+        $user->save();
+        return $user;
+    }
+
+    /**
+     * Find a user by email.
+     * @param string $email
+     * @return User|null
+     */
+    public function findByEmail(string $email): ?User
+    {
+        return User::where('email', $email)->first();
+    }
+
+    /**
+     * Delete a user.
+     * @param User $user
+     * @return void
+     */
+    public function delete(User $user): void
+    {
+        $user->delete();
+    }
+}
