9247b3120a4de4d961880b4d6f6f5df257a41484
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-09 23:59:27 -0300

    fix: :bug: correct password field access error during login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,7 +36,7 @@ class AuthController extends Controller
 
         $user = User::where('email', $request->input('email'))->first();
 
-        if (! $user || ! Hash::check($request->input('password'), $user->input('password'))) {
+        if (! $user || ! Hash::check($request->input('password'), $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
