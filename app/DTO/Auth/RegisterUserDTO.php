c83d765f184fb230546e673db59b19816a5119f0
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 09:20:00 -0300

    feat: :sparkles: add RegisterUserDTO for user registration

---
+++ app/DTO/Auth/RegisterUserDTO.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DTO\Auth;
+
+/**
+ * Class RegisterUserDTO
+ * @package App\DTO\Auth
+ */
+class RegisterUserDTO
+{
+    /**
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * @var string
+     */
+    public string $email;
+
+    /**
+     * @var string
+     */
+    public string $password;
+
+    /**
+     * RegisterUserDTO constructor.
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     */
+    public function __construct(string $name, string $email, string $password)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+}
