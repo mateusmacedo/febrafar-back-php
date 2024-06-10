21558c42ba7940cc0514e94bbae111c70ba55d50
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 09:31:47 -0300

    feat: ✨ add log request middleware and update kernel

--- config/app.php
+++ config/app.php
@@ -168,6 +168,12 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /**
+         * Custom Service Providers...
+         */
+        App\Providers\RepositoryServiceProvider::class,
+        App\Providers\ServiceServiceProvider::class,
     ])->toArray(),
 
     /*
