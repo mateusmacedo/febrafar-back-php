27b0b702f8d3ad2a173b551a1cd4ff0633d4b3b1
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-09 23:01:21 -0300

    refactor: :gear: restructure auth routes for clarity

--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
 
 Route::apiResource('activities', ActivityController::class);
-
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::apiResource('activities', ActivityController::class);
-});
