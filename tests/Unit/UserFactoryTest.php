1a93000d0a98839b4809ca6f0b1a9accda437203
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 09:55:59 -0300

    test: :test_tube: add unit test for UserFactory creation from DTO

---
+++ tests/Unit/UserFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\DTO\Auth\RegisterUserDTO;
+use App\Factories\UserFactory;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+
+class UserFactoryTest extends TestCase
+{
+    /**
+     * Test the createFromDTO method.
+     */
+    public function test_create_from_dto(): void
+    {
+        $userFactory = new UserFactory();
+
+        $registerUserDTO = new RegisterUserDTO(
+            'Test User',
+            'test@email.com',
+            'password123',
+        );
+
+        $user = $userFactory->createFromDTO($registerUserDTO);
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($registerUserDTO->name, $user->name);
+        $this->assertEquals($registerUserDTO->email, $user->email);
+        $this->assertTrue(Hash::check($registerUserDTO->password, $user->password));
+    }
+}
