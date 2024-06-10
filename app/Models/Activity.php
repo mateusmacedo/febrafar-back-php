fab3a0f1535ad9e7797fd4f90103572328e5b7b8
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 04:00:18 -0300

    docs: :books: add properties, constants, and casts to Activity model
    Expanded the Activity model with new properties, constants, and type casts to better document and handle activity attributes.

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -5,15 +5,36 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 /**
+ * Activity model.
+ *
+ * @property int $id
  * @property string $title
+ * @property string $type
+ * @property string|null $description
+ * @property int $user_id
+ * @property Carbon $start_date
+ * @property Carbon $due_date
+ * @property Carbon|null $completion_date
+ * @property string $status
+ * @property-read User $user
  *
  * @method static Builder where($column, $operator = null, $value = null, $boolean = 'and')
+ * @method static Builder whereBetween($column, array $values, $boolean = 'and', $not = false)
  */
 class Activity extends Model
 {
     use HasFactory;
+
+    const UNAUTHORIZED = 'unauthorized';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'title',
         'type',
@@ -25,6 +46,22 @@ class Activity extends Model
         'status',
     ];
 
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string>
+     */
+    protected $casts = [
+        'start_date' => 'datetime',
+        'due_date' => 'datetime',
+        'completion_date' => 'datetime',
+    ];
+
+    /**
+     * Get the user that owns the activity.
+     *
+     * @return BelongsTo
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
