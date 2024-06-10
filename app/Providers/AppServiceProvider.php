fda1da35eba54fcac788b09ea21f8c82f78ef53d
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-09 23:45:27 -0300

    refactor: :hammer: update code for consistency and clarity

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->isLocal()) {
+        if (app()->environment('local')) {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
         //
