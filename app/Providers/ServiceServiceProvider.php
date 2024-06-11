5fb8bdc2e76a8d5062185ad4b72c9342874ed1e2
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 21:09:26 -0300

    feat: :sparkles: add bindings for activity and auth services
    Added bindings for various Activity and Auth services and factories to streamline dependency injection and enhance modularity.

--- app/Providers/ServiceServiceProvider.php
+++ app/Providers/ServiceServiceProvider.php
@@ -2,12 +2,24 @@
 
 namespace App\Providers;
 
+use App\Factories\Activity\ActivityFactory;
+use App\Factories\Activity\ActivityIndexDTOFactory;
+use App\Factories\Activity\ActivityStoreDTOFactory;
+use App\Factories\Activity\ActivityUpdateDTOFactory;
+use App\Factories\Activity\Contracts\ActivityFactoryInterface;
+use App\Factories\Activity\Contracts\ActivityIndexDTOFactoryInterface;
+use App\Factories\Activity\Contracts\ActivityStoreDTOFactoryInterface;
+use App\Factories\Activity\Contracts\ActivityUpdateDTOFactoryInterface;
 use App\Factories\AuthCredentialsDTOFactory;
 use App\Factories\Contracts\AuthCredentialsDTOFactoryInterface;
+use App\Factories\Contracts\LogoutDTOFactoryInterface;
 use App\Factories\Contracts\RegisterUserDTOFactoryInterface;
 use App\Factories\Contracts\UserFactoryInterface;
+use App\Factories\LogoutDTOFactory;
 use App\Factories\RegisterUserDTOFactory;
 use App\Factories\UserFactory;
+use App\Services\Activity\ActivityService;
+use App\Services\Activity\Contracts\ActivityServiceInterface;
 use App\Services\AuthService;
 use App\Services\Contracts\AuthServiceInterface;
 use Illuminate\Support\ServiceProvider;
@@ -21,8 +33,14 @@ class ServiceServiceProvider extends ServiceProvider
     {
         $this->app->bind(AuthServiceInterface::class, AuthService::class);
         $this->app->bind(UserFactoryInterface::class, UserFactory::class);
-        $this->app->bind(RegisterUserDTOFactoryInterface::class, RegisterUserDTOFactory::class);
         $this->app->bind(AuthCredentialsDTOFactoryInterface::class, AuthCredentialsDTOFactory::class);
+        $this->app->bind(ActivityServiceInterface::class, ActivityService::class);
+        $this->app->bind(RegisterUserDTOFactoryInterface::class, RegisterUserDTOFactory::class);
+        $this->app->bind(LogoutDTOFactoryInterface::class, LogoutDTOFactory::class);
+        $this->app->bind(ActivityIndexDTOFactoryInterface::class, ActivityIndexDTOFactory::class);
+        $this->app->bind(ActivityStoreDTOFactoryInterface::class, ActivityStoreDTOFactory::class);
+        $this->app->bind(ActivityUpdateDTOFactoryInterface::class, ActivityUpdateDTOFactory::class);
+        $this->app->bind(ActivityFactoryInterface::class, ActivityFactory::class);
     }
 
     /**
