83c41b29de6084edbe4bd60a5d59cee72d8aea09
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 21:07:40 -0300

    feat: :sparkles: add activity repository interface implementation
    Introduced ActivityRepository and its interface to handle activity data operations, improving data management and separation of concerns.

---
+++ app/Repositories/Activity/Contracts/ActivityRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Activity\Contracts;
+
+use App\DTO\Activity\ActivityIndexDTO;
+use App\DTO\Activity\ActivityShowDTO;
+use App\Models\Activity;
+use Illuminate\Support\Collection;
+
+interface ActivityRepositoryInterface
+{
+    public function findByDateRange(ActivityIndexDTO $dateRange): Collection;
+    public function findById(ActivityShowDTO $dto): ?Activity;
+    public function save(Activity $activity): Activity;
+    public function delete(Activity $activity): void;
+}
