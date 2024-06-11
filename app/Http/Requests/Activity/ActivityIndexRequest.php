f3baa34ffb00cafc1c2a76abd36b3870fe70841a
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-06-10 22:42:35 -0300

    docs: :books: add OpenAPI schemas for activity requests
    refactor: :hammer: make properties readonly in DTOs for immutability

--- app/Http/Requests/Activity/ActivityIndexRequest.php
+++ app/Http/Requests/Activity/ActivityIndexRequest.php
@@ -4,6 +4,28 @@ namespace App\Http\Requests\Activity;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+/**
+ * @OA\Schema(
+ *     schema="ActivityIndexRequest",
+ *     required={"start_date", "end_date"},
+ *     @OA\Property(
+ *         property="start_date",
+ *         type="string",
+ *         format="date",
+ *         description="The start date of the activity period",
+ *         example="2024-01-01",
+ *         default="2024-01-01"
+ *     ),
+ *     @OA\Property(
+ *         property="end_date",
+ *         type="string",
+ *         format="date",
+ *         description="The end date of the activity period",
+ *         example="2024-12-31",
+ *         default="2024-12-31"
+ *     ),
+ * )
+ */
 class ActivityIndexRequest extends FormRequest
 {
     /**
